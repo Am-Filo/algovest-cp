d7488f3df5963c472fbc43741b898f9566fda64f
Amary <laumiqv@gmail.com>
2021-02-17 22:26:18 +0300

    upd: code comments

--- src/app/service/contract/contract.service.ts
+++ src/app/service/contract/contract.service.ts
@@ -40,6 +40,13 @@ export class ContractService {
 
   constructor(private httpService: HttpClient, private config: AppConfig) {}
 
+  /**
+   * Main Site Information
+   * @description Get main information for site from contract.
+   * @example
+   * contractService.getMainInfo().then((info) => {console.log('info',info);});
+   * @returns info:{totalAvs,totalStakedAVS,totalStakers,balance}
+   */
   public async getMainInfo(): Promise<any> {
     const promises = [
       this.getTotalAvs().then((value) => {
@@ -48,13 +55,13 @@ export class ContractService {
           value,
         };
       }),
-      this.totalStakedAVS().then((value) => {
+      this.getTotalStakedAVS().then((value) => {
         return {
           key: 'totalStakedAVS',
           value,
         };
       }),
-      this.totalStakers().then((value) => {
+      this.getTotalStakers().then((value) => {
         return {
           key: 'totalStakers',
           value,
@@ -83,21 +90,13 @@ export class ContractService {
     });
   }
 
-  public getAvsAddress(): Promise<any> {
-    return this.StakingContract.methods
-      .avsAddress()
-      .call()
-      .then(
-        (value: any) => {
-          this.avsAddress = value;
-          return value;
-        },
-        (err: any) => {
-          console.log('getTotalAvs', err);
-        }
-      );
-  }
-
+  /**
+   * Get Total AVS (Coin)
+   * @description Get information about total AVS coins on contract.
+   * @example
+   * contractService.getTotalAvs().then((totalAvs) => {console.log('totalAvs',totalAvs);});
+   * @returns totalAvs: number
+   */
   public getTotalAvs(): Promise<any> {
     return this.TokenContract.methods
       .totalSupply()
@@ -112,21 +111,14 @@ export class ContractService {
       );
   }
 
-  public getFreezedAVSTokens(): Promise<any> {
-    return this.StakingContract.methods
-      .freezedAVSTokens()
-      .call()
-      .then(
-        (value: any) => {
-          return value;
-        },
-        (err: any) => {
-          console.log('getFreezedAVSTokens', err);
-        }
-      );
-  }
-
-  public totalStakedAVS(): Promise<any> {
+  /**
+   * Get Total Staked AVS (Coin)
+   * @description Get information about total staked coins on contract.
+   * @example
+   * contractService.totalStakedAVS().then((totalStakedAVS) => {console.log('totalStakedAVS',totalStakedAVS);});
+   * @returns totalStakedAVS: number
+   */
+  public getTotalStakedAVS(): Promise<any> {
     return this.StakingContract.methods
       .totalStakedAVS()
       .call()
@@ -135,12 +127,19 @@ export class ContractService {
           return value;
         },
         (err: any) => {
-          console.log('totalStakedAVS', err);
+          console.log('getTotalStakedAVS', err);
         }
       );
   }
 
-  public totalStakers(): Promise<any> {
+  /**
+   * Get Total Stakers
+   * @description Get information about total contract stakers.
+   * @example
+   * contractService.totalStakers().then((totalStakers) => {console.log('totalStakers',totalStakers);});
+   * @returns totalStakers: number
+   */
+  public getTotalStakers(): Promise<any> {
     return this.StakingContract.methods
       .totalStakers()
       .call()
@@ -149,23 +148,55 @@ export class ContractService {
           return value;
         },
         (err: any) => {
-          console.log('totalStakers', err);
+          console.log('getTotalStakers', err);
         }
       );
   }
 
-  public getZeroDayStartTime(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.StakingContract.methods
-        .zeroDayStartTime()
-        .call()
-        .then((res: any) => {
-          console.log('zeroDayStartTime', res);
-          resolve(res);
-        });
-    });
+  /**
+   * Get Seven Days Info
+   * @description Get information about percent avarage staking.
+   * @example
+   * contractService.getSevenDays().then((percent) => {console.log('percent',percent);});
+   * @returns percent: number
+   */
+  public async getSevenDays(): Promise<any> {
+    return this.StakingContract.methods
+      .seven_days()
+      .call()
+      .then(
+        (res: any) => {
+          return res;
+        },
+        (err: any) => {
+          console.log('getSevenDays', err);
+        }
+      );
   }
 
+  /**
+   * Get Contract Timestamp
+   * @description Get timestamp from contract.
+   * @example
+   * contractService.getTimeStampFromContract().then((time) => {console.log('time',time);});
+   * @returns time: number
+   */
+  public async getTimeStampFromContract(date: number): Promise<number> {
+    return this.StakingContract.methods
+      .getDayUnixTime(date)
+      .call()
+      .then((endDay: number) => {
+        return endDay * 1000;
+      });
+  }
+
+  /**
+   * Get Account Stakes
+   * @description Get collection of account stakes from contract.
+   * @example
+   * contractService.getAccountStakes().then((stakes) => {console.log('stakes',stakes);});
+   * @returns stakes[]:{index: number,id: number,start: number,end: number,stakedAVS: number,totalReward: number,withdrawProgress: boolean,};
+   */
   public async getAccountStakes(): Promise<any> {
     return this.StakingContract.methods
       .stakeListCount(this.account.address)
@@ -181,20 +212,7 @@ export class ContractService {
               .stakeList(this.account.address, sessionId)
               .call()
               .then((oneSession) => {
-                const promises = [
-                  this.StakingContract.methods
-                    .getDayUnixTime(oneSession.startDay)
-                    .call()
-                    .then((startDay: number) => {
-                      return startDay * 1000;
-                    }),
-                  this.StakingContract.methods
-                    .getDayUnixTime(oneSession.startDay + oneSession.numDaysStake)
-                    .call()
-                    .then((endDay: number) => {
-                      return endDay * 1000;
-                    }),
-                ];
+                const promises = [this.getTimeStampFromContract(oneSession.startDay), this.getTimeStampFromContract(oneSession.startDay + oneSession.numDaysStake)];
 
                 return Promise.all(promises).then((stake) => {
                   return {
@@ -222,29 +240,13 @@ export class ContractService {
       });
   }
 
-  public async getSevenDays(): Promise<any> {
-    return this.StakingContract.methods
-      .seven_days()
-      .call()
-      .then((res: any) => {
-        return res;
-      });
-  }
-
-  public getDayDurationSec(): Promise<any> {
-    return this.StakingContract.methods
-      .dayDurationSec()
-      .call()
-      .then(
-        (res: any) => {
-          return res;
-        },
-        (err: any) => {
-          console.log('err', err);
-        }
-      );
-  }
-
+  /**
+   * Check Allowance
+   * @description Check if address accept spend amount of coins to contract.
+   * @example
+   * contractService.getAllowance(amount).then(() => {},() => {});
+   * @returns true | false
+   */
   private getAllowance(amount: string | number): Promise<any> {
     return new Promise((resolve, reject) => {
       this.TokenContract.methods
@@ -258,17 +260,13 @@ export class ContractService {
     });
   }
 
-  public async unstake(sessionId: number, id: number): Promise<any> {
-    return this.StakingContract.methods
-      .stakeEnd(sessionId, id)
-      .send({
-        from: this.account.address,
-      })
-      .then((res) => {
-        return this.checkTransaction(res);
-      });
-  }
-
+  /**
+   * Start Stake
+   * @description Send coins to staking on contract.
+   * @example
+   * contractService.startStake(amount, day).then(() => {}).catch((err)=>{});
+   * @returns true | false
+   */
   public startStake(amount: string | number, day: number): Promise<any> {
     const fromAccount = this.account.address;
 
@@ -303,29 +301,46 @@ export class ContractService {
     });
   }
 
-  public currentDay(): Promise<any> {
+  /**
+   * Unstake
+   * @description Retrive coins from staking.
+   * @example
+   * contractService.unstake(sessionId,id).then(()=>{console.log('complete');}).catch(() => {console.log('error');});
+   * @returns true | false
+   */
+  public async unstake(sessionId: number, id: number): Promise<any> {
     return this.StakingContract.methods
-      .currentDay()
-      .call()
-      .then(
-        (res: any) => {
-          return res;
-        },
-        (err: any) => {
-          console.log('err', err);
-        }
-      );
+      .stakeEnd(sessionId, id)
+      .send({
+        from: this.account.address,
+      })
+      .then((res: any) => {
+        return this.checkTransaction(res);
+      });
   }
 
-  public async getTokenBalance(address?: string): Promise<any> {
+  /**
+   * Token Balance
+   * @description Get token contract balance on eth metamask address.
+   * @example
+   * contractService.getTokenBalance(address).then((balance)=>{console.log('balance',balance)});
+   * @returns balance: string | number
+   */
+  public async getTokenBalance(address?: string): Promise<string | number> {
     return this.TokenContract.methods
       .balanceOf(address ? address : this.account.address)
       .call()
-      .then((balance: any) => {
+      .then((balance: string | number) => {
         return balance;
       });
   }
 
+  /**
+   * Check Transaction
+   * @description Checking transaction in blockchain.
+   * @example
+   * new Promise((resolve, reject) => {contractService.checkTx(tx, resolve, reject);});
+   */
   private checkTx(tx: any, resolve: any, reject: any): void {
     this.metaMaskWeb3.Web3.eth.getTransaction(tx.transactionHash).then((txInfo) => {
       if (txInfo.blockNumber) {
@@ -339,18 +354,38 @@ export class ContractService {
     }, reject);
   }
 
-  private callAllTransactionsSubscribers(transaction: any): void {
+  /**
+   * Check All Transaction Subscribers
+   * @description Trigger observer subscribers if transaction complete.
+   * @example
+   * contractService.callAllTransactionsSubscribers(txInfo);
+   */
+  private callAllTransactionsSubscribers(transaction: string): void {
     this.allTransactionSubscribers.forEach((observer) => {
       observer.next(transaction);
     });
   }
 
-  private checkTransaction(tx: any): Promise<any> {
+  /**
+   * Check Transaction
+   * @description Start checking transaction.
+   * @example
+   * contractService.checkTransaction(tx);
+   * @returns value from checkTx() function
+   */
+  private checkTransaction(tx: string): Promise<any> {
     return new Promise((resolve, reject) => {
       this.checkTx(tx, resolve, reject);
     });
   }
 
+  /**
+   * Transaction Subscribe
+   * @description Create new Observer of transactions which will add it to allTransactionSubscribers variable and return information transaction failed or successed.
+   * @example
+   * contractService.transactionsSubscribe().subscribe((transaction) => {console.log('success', transaction)});
+   * @returns transaction: {tx: string}
+   */
   public transactionsSubscribe(): Observable<any> {
     const newObserver = new Observable((observer) => {
       this.allTransactionSubscribers.push(observer);
@@ -358,27 +393,46 @@ export class ContractService {
     return newObserver;
   }
 
+  /**
+   * Static information
+   * @description Main initialized function which will initialized MetaMaskService, retrive account information and triggered getContractsInfo() function.
+   * @example
+   * contractService.getStaticInfo().then(() => {}).catch((err)=>{});
+   * @returns true
+   */
   public async getStaticInfo(): Promise<any> {
     return this.initAll().then(() => {
       this.metaMaskWeb3 = new MetamaskService(this.config);
       this.metaMaskWeb3.getAccounts().subscribe((account: any) => {
         if (account) {
-          const promises = [this.getTokensInfo(false)];
+          const promises = [this.getContractsInfo(false)];
           return Promise.all(promises);
         }
       });
     });
   }
 
-  private getTokensInfo(noEnable?: boolean): Promise<any> {
+  /**
+   * Get contract information.
+   * @description Triggered initializeContracts() function than will add contracts abi and address to metamask configuration.
+   * @example
+   * contractService.getContractsInfo(false).then(() => {});
+   * @returns true
+   */
+  private getContractsInfo(noEnable?: boolean): Promise<any> {
     if (!noEnable) {
       this.initializeContracts();
     }
-
     const promises = [true];
     return Promise.all(promises);
   }
 
+  /**
+   * Application initialized
+   * @description Load setting and contracrs information via https from assets/js/. Set application setting and contracts.
+   * @example
+   * contractService.initAll().then(() => {});
+   */
   private async initAll(): Promise<any> {
     const promises = [
       this.httpService
@@ -407,16 +461,35 @@ export class ContractService {
     });
   }
 
+  /**
+   * Load account information
+   * @description Triggered function callAllAccountsSubscribers.
+   * @example
+   * contractService.loadAccountInfo();
+   */
   public loadAccountInfo(): any {
     this.callAllAccountsSubscribers();
   }
 
+  /**
+   * All Account Subscribers
+   * @description Send new account value to all account subscribers.
+   * @example
+   * contractService.callAllAccountsSubscribers();
+   */
   private callAllAccountsSubscribers(): void {
     this.allAccountSubscribers.forEach((observer) => {
       observer.next(this.account);
     });
   }
 
+  /**
+   * Get account information.
+   * @description Retrive infromation about account address via MetaMaskService from blockchain by address and get token address balance.
+   * @example
+   * contractService.getAccount().then((account) => { console.log(account); }).catch((err) => { console.log(err); });
+   * @returns account: {address: string, balance: string | number}
+   */
   public getAccount(noEnable?: boolean): Promise<any> {
     return new Promise((resolve, reject) => {
       this.metaMaskWeb3.getAccounts(noEnable).subscribe(
@@ -441,31 +514,35 @@ export class ContractService {
     });
   }
 
+  /**
+   * Getting Token Address
+   * @description Return adress of token contract.
+   * @example
+   * contractService.getContractTokenAddress();
+   * @returns address
+   */
   public getContractTokenAddress(): string {
     return this.tokenAddress;
   }
 
+  /**
+   * Getting Token Address
+   * @description Return adress of token contract.
+   * @example
+   * contractService.getContractTokenAddress();
+   * @returns address
+   */
   public getStakingAddress(): string {
     return this.stakingAddress;
   }
 
-  public accountSubscribe(): Observable<any> {
-    const newObserver = new Observable((observer) => {
-      observer.next(this.account);
-      this.allAccountSubscribers.push(observer);
-      return {
-        unsubscribe: () => {
-          this.allAccountSubscribers = this.allAccountSubscribers.filter((a) => a !== newObserver);
-        },
-      };
-    });
-    return newObserver;
-  }
-
+  /**
+   * Initialize contracts.
+   * @description Add your variable of contract to load it
+   */
   private initializeContracts(): void {
     this.StakingContract = this.metaMaskWeb3.getContract(this.CONTRACTS_PARAMS.Staking.ABI, this.CONTRACTS_PARAMS.Staking.ADDRESS);
     this.TokenContract = this.metaMaskWeb3.getContract(this.CONTRACTS_PARAMS.Token.ABI, this.CONTRACTS_PARAMS.Token.ADDRESS);
-
     this.tokenAddress = this.CONTRACTS_PARAMS.Token.ADDRESS;
     this.stakingAddress = this.CONTRACTS_PARAMS.Staking.ADDRESS;
   }
